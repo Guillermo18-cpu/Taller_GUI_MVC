5a9edfb6f05bd19c097a501a69fe23e54dcdbe73
Santi104 <monzonedi05@gmail.com>
2025-05-20 18:38:39 -0500

    Se modifico el Main para que todas las partes sean funcionales

--- src/Vista/Main.java
+++ src/Vista/Main.java
@@ -1,57 +1,81 @@
 package vista;
-
 import javax.swing.*;
 import java.awt.*;
+import vista.*;
+
+import controlador.*;
 
 public class Main extends JFrame {
-
-    private JMenuBar barraMenu;
-    private JMenu menuGestion;
-    private JMenuItem itemEstudiantes, itemDocentes, itemCursos, itemMatricula, itemReporte;
 
     public Main() {
         setTitle("Gestión Academica");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
         pack();
         setExtendedState(JFrame.MAXIMIZED_BOTH);
-        
 
         initMenu();
     }
 
     private void initMenu() {
-        barraMenu = new JMenuBar();
-        menuGestion = new JMenu("Gestión");
+        JMenuBar barraMenu = new JMenuBar();
+        JMenu menu = new JMenu("Gestión");
 
-        itemEstudiantes = new JMenuItem("Estudiantes");
-        itemDocentes = new JMenuItem("Docentes");
-        itemCursos = new JMenuItem("Cursos");
-        itemMatricula = new JMenuItem("Matrícula");
-        itemReporte = new JMenuItem("Reporte");
+        JMenuItem itemEstudiante = new JMenuItem("Estudiantes");
+        JMenuItem itemDocente = new JMenuItem("Docentes");
+        JMenuItem itemCurso = new JMenuItem("Cursos");
+        JMenuItem itemMatricula = new JMenuItem("Matrícula");
+        JMenuItem itemReporte = new JMenuItem("Reporte");
 
-        menuGestion.add(itemEstudiantes);
-        menuGestion.add(itemDocentes);
-        menuGestion.add(itemCursos);
-        menuGestion.add(itemMatricula);
-        menuGestion.add(itemReporte);
+        menu.add(itemEstudiante);
+        menu.add(itemDocente);
+        menu.add(itemCurso);
+        menu.add(itemMatricula);
+        menu.add(itemReporte);
 
-        barraMenu.add(menuGestion);
+        barraMenu.add(menu);
         setJMenuBar(barraMenu);
 
-        itemEstudiantes.addActionListener(e -> cambiarPanel(new EstudianteVista()));
-        itemDocentes.addActionListener(e -> cambiarPanel(new DocenteVista()));
-        itemCursos.addActionListener(e -> cambiarPanel(new VistaCurso()));
-        itemMatricula.addActionListener(e -> cambiarPanel(new VistaMatricula()));
-        itemReporte.addActionListener(e -> cambiarPanel(new ReporteVista()));
+        itemEstudiante.addActionListener(e -> {
+            EstudianteVista vista = new EstudianteVista();
+            new EstudianteControlador(vista);
+            setPanel(vista);
+        });
+
+        itemDocente.addActionListener(e -> {
+            DocenteVista vista = new DocenteVista();
+            new DocenteControlador(vista);
+            setPanel(vista);
+        });
+
+        itemCurso.addActionListener(e -> {
+            VistaCurso vista = new VistaCurso();
+            new CursoControlador(vista);
+            setPanel(vista);
+        });
+
+        itemMatricula.addActionListener(e -> {
+            VistaMatricula vista = new VistaMatricula();
+            new MatriculaControlador(vista);
+            setPanel(vista);
+        });
+
+        itemReporte.addActionListener(e -> {
+            ReporteVista vista = new ReporteVista();
+            new ReporteControlador(vista);
+            setPanel(vista);
+        });
     }
 
-    private void cambiarPanel(JPanel panel1) {
+    private void setPanel(JPanel panel) {
         getContentPane().removeAll();
-        getContentPane().add(panel1, BorderLayout.CENTER);
+        getContentPane().add(panel, BorderLayout.CENTER);
         revalidate();
         repaint();
     }
+    
+    
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
